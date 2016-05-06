25640210067aaefbc9d2c0a3f93ce420611b3c9e
prttstft <prttstft@gmail.com>
2016-05-06 04:34:32 +0200

    Added API link + smaller stuff.

--- app/src/main/java/de/prttstft/materialmensa/ui/activities/about/AboutActivity.java
+++ app/src/main/java/de/prttstft/materialmensa/ui/activities/about/AboutActivity.java
@@ -1,8 +1,10 @@
 package de.prttstft.materialmensa.ui.activities.about;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.v4.content.ContextCompat;
@@ -22,12 +24,14 @@ import de.prttstft.materialmensa.extras.Utilities;
 
 public class AboutActivity extends AppCompatActivity {
     @SuppressWarnings("SpellCheckingInspection") private static final String CHROME_PACKAGE = "com.android.chrome";
+    @SuppressWarnings("SpellCheckingInspection") private static final Uri API_URL = Uri.parse("http://www.studentenwerk-pb.de/");
     @SuppressWarnings("SpellCheckingInspection") private static final Uri EMAIL = Uri.parse("mailto:prttstft.dev@gmail.com");
     @SuppressWarnings("SpellCheckingInspection") private static final Uri EMOJI_ONE_URL = Uri.parse("http://emojione.com/");
     @SuppressWarnings("SpellCheckingInspection") private static final Uri GIT_HUB_REPO_URL = Uri.parse("https://github.com/prttstft/MaterialMensa");
     @SuppressWarnings("SpellCheckingInspection") private static final Uri ICONS_EIGHT_URL = Uri.parse("https://icons8.com/");
     @SuppressWarnings("SpellCheckingInspection") private static final Uri PLAY_STORE_URL = Uri.parse("https://goo.gl/HD2ed2");
     @SuppressWarnings("SpellCheckingInspection") private static final Uri WISH_LIST_URL = Uri.parse("https://www.amazon.de/registry/wishlist/2RQNQ7DEKD9WF");
+    @SuppressWarnings({"WeakerAccess", "unused"}) @Bind(R.id.activity_about_button_api) AppCompatButton buttonApi;
     @SuppressWarnings({"WeakerAccess", "unused"}) @Bind(R.id.activity_about_button_emojis) AppCompatButton buttonEmojis;
     @SuppressWarnings({"WeakerAccess", "unused"}) @Bind(R.id.activity_about_button_git_hub) AppCompatButton buttonGitHub;
     @SuppressWarnings({"WeakerAccess", "unused"}) @Bind(R.id.activity_about_button_icons) AppCompatButton buttonIcons;
@@ -56,6 +60,13 @@ public class AboutActivity extends AppCompatActivity {
     }
 
     private void setUpOnClickListeners() {
+        buttonApi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startCustomTab(API_URL);
+            }
+        });
+
         buttonEmojis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,7 +91,7 @@ public class AboutActivity extends AppCompatActivity {
         buttonRate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startCustomTab(PLAY_STORE_URL);
+                rateApp();
             }
         });
 
@@ -139,5 +150,27 @@ public class AboutActivity extends AppCompatActivity {
         } catch (PackageManager.NameNotFoundException e) {
             return false;
         }
+    }
+
+    public void rateApp() {
+        try {
+            Intent rateIntent = startPlayStoreIntent("market://details");
+            startActivity(rateIntent);
+        } catch (ActivityNotFoundException e) {
+            startCustomTab(PLAY_STORE_URL);
+        }
+    }
+
+    private Intent startPlayStoreIntent(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.format("%s?id=%s", url, getPackageName())));
+        int flags = Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_MULTIPLE_TASK;
+        if (Build.VERSION.SDK_INT >= 21) {
+            flags |= Intent.FLAG_ACTIVITY_NEW_DOCUMENT;
+        } else {
+            //noinspection deprecation
+            flags |= Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET;
+        }
+        intent.addFlags(flags);
+        return intent;
     }
 }
